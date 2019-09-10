699236f9e9402c157ed8386d0be3723ad00ab740
Joey <woqiaoxun@163.com>
2019-09-10 22:47:15 +0800

    hello world

---
+++ src/main/java/com/study/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/world")
+    public String world() {
+        return "hello world";
+    }
+}
