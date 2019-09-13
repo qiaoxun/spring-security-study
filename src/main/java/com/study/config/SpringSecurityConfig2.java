d6314ed7067d809c599059970bdb4585ca681d29
Joey <woqiaoxun@163.com>
2019-09-13 17:24:56 +0800

    add success and failure handler

--- src/main/java/com/study/config/SpringSecurityConfig2.java
+++ src/main/java/com/study/config/SpringSecurityConfig2.java
@@ -1,5 +1,7 @@
 package com.study.config;
 
+import com.study.authentication.MyAuthenticationFailureHandler;
+import com.study.authentication.MyAuthenticationSuccessHandler;
 import com.study.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -14,11 +16,19 @@ public class SpringSecurityConfig2 extends WebSecurityConfigurerAdapter {
     @Autowired
     private SecurityProperties securityProperties;
 
+    @Autowired
+    private MyAuthenticationSuccessHandler myAuthenticationSuccessHandler;
+
+    @Autowired
+    private MyAuthenticationFailureHandler myAuthenticationFailureHandler;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .loginPage("/authentication/require")
                 .loginProcessingUrl("/authentication/form")
+                .successHandler(myAuthenticationSuccessHandler)
+                .failureHandler(myAuthenticationFailureHandler)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/authentication/require",  securityProperties.getBrowser().getLoginPage()).permitAll()
---
+++ src/main/java/com/study/utils/LoginType.java
@@ -0,0 +1,4 @@
+package com.study.utils;
+
+public enum LoginType {
+}
