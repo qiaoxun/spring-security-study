321852a361f93d348ef447bd6315f42a381fc0db
Joey <woqiaoxun@163.com>
2019-09-15 23:19:00 +0800

    add remember me

--- src/main/java/com/study/config/SpringSecurityConfig2.java
+++ src/main/java/com/study/config/SpringSecurityConfig2.java
@@ -3,13 +3,17 @@ package com.study.config;
 import com.study.authentication.MyAuthenticationFailureHandler;
 import com.study.authentication.MyAuthenticationSuccessHandler;
 import com.study.filter.VerifyCodeFilter;
-import com.study.filter.VerifyCodeFilter_V1;
+import com.study.login.MyUserDetailsService;
 import com.study.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 @Configuration
 //@EnableConfigurationProperties(SecurityProperties.class)
@@ -23,6 +27,12 @@ public class SpringSecurityConfig2 extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyAuthenticationFailureHandler myAuthenticationFailureHandler;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private MyUserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -42,6 +52,10 @@ public class SpringSecurityConfig2 extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/authentication/logout")
                 .logoutSuccessUrl(securityProperties.getBrowser().getLoginPage())
                 .and()
+                .rememberMe()
+                .tokenRepository(persistentTokenRepository())
+                .userDetailsService(userDetailsService)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/authentication/require", "/authentication/verifyCode",  securityProperties.getBrowser().getLoginPage()).permitAll()
                 .anyRequest()
@@ -50,4 +64,12 @@ public class SpringSecurityConfig2 extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable();
     }
+
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        jdbcTokenRepository.setCreateTableOnStartup(true);
+        return jdbcTokenRepository;
+    }
+
 }
