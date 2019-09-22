8dc34c6fb1ce5d6df29464844bf31df2ed51c963
Joey <woqiaoxun@163.com>
2019-09-22 23:03:52 +0800

    @PreAuthorize test

--- src/main/java/com/study/controller/HelloController.java
+++ src/main/java/com/study/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.study.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,11 @@ public class HelloController {
     @RequestMapping("/world")
     public String world() {
         return "hello world";
+    }
+
+    @RequestMapping("/preAuthorizeTest")
+    @PreAuthorize("hasAnyRole('ADMIN','DICT_ALL','DICT_EDIT')")
+    public String preAuthorizeTest() {
+        return "preAuthorizeTest";
     }
 }
