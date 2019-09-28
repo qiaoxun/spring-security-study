379bf306bf72dd8d47b6b7dc360512880566af54
Joey <woqiaoxun@163.com>
2019-09-28 23:25:38 +0800

    admin to ADMIN

--- src/main/java/com/study/controller/HelloController.java
+++ src/main/java/com/study/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     }
 
     @RequestMapping("/preAuthorizeTest")
-    @PreAuthorize("hasAnyRole('admin', 'DICT_ALL','DICT_EDIT')")
+    @PreAuthorize("hasAnyRole('ADMIN', 'DICT_ALL','DICT_EDIT')")
     public String preAuthorizeTest() {
         return "preAuthorizeTest";
     }
